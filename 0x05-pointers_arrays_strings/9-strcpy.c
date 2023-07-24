4604a6a8d846ba22575d01c281f52042a3065291
Blessing011 <chiazorblessing00@gmail.com>
2023-07-24 14:42:43 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcpy - Copy a string
+ * @dest: Destination value
+ * @src: Source value
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int 1 = 0;
+	int z = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		1++;
+	}
+	for (; x < 1 ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[1] = '\0';
+	return (dest);
+}
